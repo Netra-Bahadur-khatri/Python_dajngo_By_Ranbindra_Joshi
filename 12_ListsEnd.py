1b31b3ef3111c3759eb83864fc57694b45772515
Netra Kc <71728112+Netra-Bahadur-khatri@users.noreply.github.com>
2021-02-19 15:31:36 +0545

    Add files via upload

---
+++ 12_ListsEnd.py
@@ -0,0 +1,17 @@
+# Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
+
+
+
+def list_end():
+    a = [5,10,15,20,25]
+    new_list = []
+    new_list.append(a[0])
+    new_list.append(a[-1])
+    return new_list
+    
+
+if __name__ == "__main__":
+    b = list_end()
+    print(b)
+
+
